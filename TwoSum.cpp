26958f8450b1ab0f4d5f8dce3023792cc80c2076
dhriti09 <DHRITIYADAV0919@GMAIL.COM>
2026-01-20 19:06:58 +0530

    Add files via upload

---
+++ TwoSum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+vector<int> twoSum(vector<int>& nums, int target) {
+   unordered_map<int, int> numToIndex; 
+   for (int i = 0; i < nums.size(); i++) {
+       int complement = target - nums[i];
+       if (numToIndex.find(complement) != numToIndex.end()) {
+           return {numToIndex[complement], i};
+       }
+       numToIndex[nums[i]] = i;
+   }
+   return {};
+}
+int main() {
+   vector<int> nums = {2, 7, 11, 15};
+   int target = 9;
+   vector<int> result = twoSum(nums, target);
+   cout << "[" << result[0] << ", " << result[1] << "]" << endl; 
+}
